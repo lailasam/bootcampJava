0321016cd12cffcedd2ab232bedf3df3a1107511
lailasam <samaralailaa@gmail.com>
2025-08-09 20:03:47 -0300

    adicionando metodos nas classes

--- interfaces/NavegadorInternet.java
+++ interfaces/NavegadorInternet.java
@@ -1,5 +1,13 @@
 package interfaces;
 
-public class NavegadorInternet {
-
+public class NavegadorInternet extends Iphone{
+public void exibirPagina(String url) {
+    System.out.println("Exibindo a página: " + url);
+}
+public void adicionarNovaAba(){
+    System.out.println("Adicionando uma nova aba");
+}
+public void atualizarPagina() {
+    System.out.println("Atualizando a página atual");
+}
 }
--- interfaces/ReprodutorMusical.java
+++ interfaces/ReprodutorMusical.java
@@ -1,5 +1,13 @@
 package interfaces;
 
 public class ReprodutorMusical extends Iphone {
-   
+   public void tocar(){
+         System.out.println("Tocando música");
+   }
+   public void pausar(){
+         System.out.println("Pausando música");
+   }
+   public void selecionarMusica(String musica){
+         System.out.println("Selecionando a música: " + musica);
+   }
 }
