08effed3f5d30509f45769fbfe5d8bdaebb23c59
lailasam <samaralailaa@gmail.com>
2025-08-11 18:16:25 -0300

    ordenacao

---
+++ list/OrdenacaoNumeros.java
@@ -0,0 +1,36 @@
+package bootcampJava.list;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*Crie uma classe chamada "OrdenacaoNumeros" 
+que possui uma lista de números inteiros como atributo. Implemente os seguintes métodos:
+adicionarNumero(int numero): Adiciona um número à lista.
+ordenarAscendente(): Ordena os números da lista em ordem ascendente usando 
+a interface Comparable e a class Collections.
+ordenarDescendente(): Ordena os números da lista em ordem descendente
+usando um Comparable e a class Collections. */
+public class OrdenacaoNumeros implements Comparable<OrdenacaoNumeros> {
+private List<Integer> numeros;
+public OrdenacaoNumeros() {
+    this.numeros = new ArrayList<>();
+}
+public void adicionarNumero(int numero) {
+    numeros.add(numero);
+}
+@Override
+public int compareTo(OrdenacaoNumeros numero) {
+    return Integer.compare(this.numeros.get(0), numero.numeros.get(0));
+}
+public List<Integer> ordenarAscendente(){
+    List<Integer> numerosOrdenados = new ArrayList<>(numeros);
+    Collections.sort(numerosOrdenados);
+    return numerosOrdenados;
+}
+public List<Integer> ordenarDescendente(){
+    List<Integer> numerosOrdenados = new ArrayList<>(numeros);
+    Collections.sort(numerosOrdenados, Collections.reverseOrder());
+    return numerosOrdenados;
+}
+}
