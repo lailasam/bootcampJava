6e06312a9f98927385918ec65a25bf287697be4e
lailasam <samaralailaa@gmail.com>
2025-08-12 17:18:57 -0300

    Livraria

---
+++ list/Livro.java
@@ -0,0 +1,33 @@
+package bootcampJava.list;
+
+public class Livro implements Comparable<Livro> {
+private String titulo;
+private String autor;
+private double preco;
+public Livro(String titulo, String autor, double preco) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.preco = preco;
+}
+public String getTitulo() {
+    return titulo;
+}
+public String getAutor() {
+    return autor;
+}
+public double getPreco() {
+    return preco;
+}
+@Override
+public String toString() {
+    return "Livro{" +
+            "titulo='" + titulo + '\'' +
+            ", autor='" + autor + '\'' +
+            ", preco=" + preco +
+            '}';
+}
+@Override
+public int compareTo(Livro l) {
+    return Double.compare(this.preco, l.getPreco());
+}
+}
